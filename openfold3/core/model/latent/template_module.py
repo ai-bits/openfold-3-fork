d10b027de61855a0ed778d377f55e6f72ba39368
Geoffrey Martin-Noble <Geoffrey.Martin-Noble@amd.com>
2026-05-20 15:53:33 -0700

    Restructure chunk size settings
    This converts the configured chunk size to the *maximum* rather than the
    *minimum*. I don't think there's a clear use case for this to be the
    minimum and users were confused. This also allows us to remove the
    hardcoded values for the max with default and flash-style kernels: these
    instead are just config settings.
    
    I removed the minimum chunk size entirely, so the tuner will search all
    powers of 2 up to the maximum. I'm struggling to think of a use case
    where someone would want to limit chunks size to a minimum of 4 (I think
    this number just came from the original suggested chunk size in the
    AlphaFold 3 paper).
    
    Note that this changes the hardcoded different chunk size for diffusion
    conditioning. I'm not sure how important this is to preserve and whether
    we saw actual latency improvements from raising the chunk size here vs
    just observing that it was possible to do so. If we want different chunk
    sizes for different modules, I think we should probably thread through a
    config setting per module because having a hardcoded value here is a bit
    surprising IMO (and note that it only gets applied when tuning is turned
    on). Probably the best analog in the config would be `offload_inference`
    as a per-module setting that isn't an init arg. If more things got added
    here, IDK whether we'd want to keep this structure or perhaps mirror the
    structure of architecture.

--- openfold3/core/model/latent/template_module.py
+++ openfold3/core/model/latent/template_module.py
@@ -35,11 +35,7 @@ from openfold3.core.model.feature_embedders.template_embedders import (
 from openfold3.core.model.latent.base_blocks import PairBlock
 from openfold3.core.model.primitives import LayerNorm, Linear
 from openfold3.core.utils.checkpointing import checkpoint_blocks, checkpoint_section
-from openfold3.core.utils.chunk_utils import (
-    DEFAULT_MAX_CHUNK_SIZE,
-    FLASH_MAX_CHUNK_SIZE,
-    ChunkSizeTuner,
-)
+from openfold3.core.utils.chunk_utils import ChunkSizeTuner
 from openfold3.core.utils.tensor_utils import add
 
 
@@ -391,14 +387,10 @@ class TemplatePairStack(nn.Module):
                 or use_triton_triangle_kernels
                 or use_deepspeed_evo_attention
             )
-            max_chunk_size = (
-                FLASH_MAX_CHUNK_SIZE if use_flash_kernels else DEFAULT_MAX_CHUNK_SIZE
-            )
             tuned_chunk_size = self.chunk_size_tuner.tune_chunk_size(
                 representative_fn=blocks[0],
                 args=(t.clone(),),
-                min_chunk_size=chunk_size,
-                max_chunk_size=max_chunk_size,
+                max_chunk_size=chunk_size,
             )
             attn_chunk = (
                 tuned_chunk_size if use_flash_kernels else (tuned_chunk_size // 4)
@@ -407,7 +399,7 @@ class TemplatePairStack(nn.Module):
                 partial(
                     b,
                     chunk_size=tuned_chunk_size,
-                    _attn_chunk_size=max(chunk_size, attn_chunk),
+                    _attn_chunk_size=attn_chunk,
                 )
                 for b in blocks
             ]
